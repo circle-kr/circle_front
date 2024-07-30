5cc789814c257085ec80bfa8017c7d97802a44fa
sohyun <sohyun32253@kaywon.ac.kr>
2024-07-30 14:53:05 +0900

    Save current changes before adding Vuetify

---
+++ Desktop/kokorokara/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <v-app>
+      <v-app-bar app>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon> 
+        <v-app-bar-title>HEADER</v-app-bar-title>
+        <v-spacer></v-spacer>
+        <v-btn icon>
+          <v-icon>favorite</v-icon>
+          <v-icon>double_arrow</v-icon>
+        </v-btn>      
+      </v-app-bar>
+      <v-main>
+
+      </v-main>
+      <v-footer>
+
+      </v-footer>
+    </v-app> 
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+
+</style>
+
