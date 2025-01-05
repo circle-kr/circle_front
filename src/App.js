57405a44255603570660305da5d7233db59038e4
sohyun <sohyun32253@kaywon.ac.kr>
2025-01-05 22:49:09 +0900

    'ass'

--- src/App.js
+++ src/App.js
@@ -5,16 +5,7 @@ import Header from './components/Header';
 import Content from './components/Content';
 import SideMenu from './components/SideMenu';
 import Footer from './components/Footer';
-import JoinCircle from './components/JoinCircle'
-import MakeCircle from './components/MakeCircle'
-import JoinedCircle from './components/JoinedCircle'
-import Notification from './components/Notification'
-import Chat from './components/Chat';
-import ChatMessage from './components/ChatMessage';
-import ProfileInfo from './components/ProfileInfo'
-import Profile from './components/Profile'
-import SignIn from './components/SignIn'
-import SignUp from './components/SignUp'
+
 
 function App() {
     const [collapsed, setCollapsed] = useState(false); // 열고 닫힘 상태
@@ -30,16 +21,6 @@ function App() {
             <SideMenu collapsed={collapsed}/>
             <Routes>
               <Route path="/" element={<Content />} />
-              <Route path="/Joincircle" element={<JoinCircle />} />
-              <Route path="/MakeCircle" element={<MakeCircle />} />
-              <Route path="/JoinedCircle" element={<JoinedCircle />} />
-              <Route path="/Notification" element={<Notification />} />
-              <Route path="/Chat" element={<Chat />} />
-              <Route path="/ChatMessage" element={<ChatMessage />} />
-              <Route path="/ProfileInfo" element={<ProfileInfo />} />
-              <Route path="/Profile" element={<Profile />} />
-              <Route path="/SignIn" element={<SignIn />} />
-              <Route path="/SignUp" element={<SignUp />} />
             </Routes>
           </div>
           <Footer />
