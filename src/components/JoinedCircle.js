4409111d45a08a4503a69a7e193e28ac3155ab20
sohyun <sohyun32253@kaywon.ac.kr>
2025-01-10 17:27:05 +0900

    'bye'

--- src/components/JoinedCircle.js
+++ src/components/JoinedCircle.js
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react';
+import JoinedCircle01 from './JoinedCircle01';
 function JoinedCircle() {
+    const [selectedCircle, setSelectedCircle] = useState(null);
+  
+    const circles = [
+        { id: 1, name: '모임 1', link: '/circle1' },
+        { id: 2, name: '모임 2', link: '/circle2' },
+        { id: 3, name: '모임 3', link: '/circle3' }
+    ];
+
+    const handleClick = (circle) => {
+        setSelectedCircle(circle); // 클릭한 모임을 상태로 설정
+    };
     return(
     <main className='main'>
-        <div className='joined_circle_wrap'></div>
+        <div className='joined_circle_wrap'>
+            <h2></h2>
+            <div className='joined_circle_cont'>
+            <ul>
+                {circles.map((circle) => (
+                <li key={circle.id}>
+                    <button onClick={() => handleClick(circle)}>
+                    {circle.name}
+                    </button>
+                </li>
+                ))}
+            </ul>
+
+            {selectedCircle && (
+                <JoinedCircle01 circle={selectedCircle} />
+            )}
+            </div>
+        </div>
     </main>
     )
 }
