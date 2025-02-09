579c6fef20392d437efb602ad717ca4c98b4d66c
sohyun <sohyun32253@kaywon.ac.kr>
2025-02-09 12:34:59 +0900

    '2.9M'

--- src/components/MakeCircle.js
+++ src/components/MakeCircle.js
@@ -9,8 +9,7 @@ function MakeCircle() {
         "language", "animation", "media", "photo", "exhibition",
         "shopping", "sport", "game", "DIY", "casual",
         "music", "book", "food", "baking"
-      ];
-
+    ];
     const charateristic = [
         "exciting","active","passtionate","sociable","friendly",
         "supportive","calm","cozy","creative","innovative",
@@ -18,14 +17,14 @@ function MakeCircle() {
     return(
     <main className='main sub_main'>
         <div className='make_circle_wrap'>
-            <h2>Make <br/>
-            your circle !</h2>
-            <div className='logo_wrap'>
-                <img src={logoTxt} alt="logo" className='logo_text'/>
-                <img src={logoStar} alt="logo" className='logo_star'/>
-            </div>
-
             <div className='make_circle_cont'>
+                <h2>Make <br/>
+                your circle !</h2>
+                <div className='logo_wrap'>
+                    <img src={logoTxt} alt="logo" className='logo_text'/>
+                    <img src={logoStar} alt="logo" className='logo_star'/>
+                </div>
+
                 <form>
                     <section className='make_circle_name'>
                         <div className='make_circle_name_cont'>
@@ -47,36 +46,34 @@ function MakeCircle() {
                         </div>
                     </section> 
 
-                    <div className='layout_middle'>
-                        <section className='charateristic_of_circle input_box'>
-                            <div className='charateristic_of_circle_cont'>
-                                <h3>③ Charateristic of circle <br/>
-                                <span>(multiple options available)</span></h3>
-                                {charateristic.map(charateristic => (
-                                <div key={charateristic}>
-                                    <input type="checkbox" id={charateristic} name="charateristic" value={charateristic} />
-                                    <label htmlFor={charateristic}>{charateristic}</label>
-                                </div>
-                                ))}
+                    <section className='charateristic_of_circle input_box'>
+                        <div className='charateristic_of_circle_cont'>
+                            <h3>③ Charateristic of circle <br/>
+                            <span>(multiple options available)</span></h3>
+                            {charateristic.map(charateristic => (
+                            <div key={charateristic}>
+                                <input type="checkbox" id={charateristic} name="charateristic" value={charateristic} />
+                                <label htmlFor={charateristic}>{charateristic}</label>
                             </div>
-                        </section>  
+                            ))}
+                        </div>
+                    </section>  
 
-                        <section className='mc_notification'>
-                            <div className='make_circle_notification_cont'>
-                                <h3>④ Notification</h3>
-                                <textarea id='notification' rows="5" placeholder='Enter 200 characters or less'></textarea>
-                                <label htmlFor='notification'></label>
-                            </div>
-                        </section>
+                    <section className='mc_notification'>
+                        <div className='make_circle_notification_cont'>
+                            <h3>④ Notification</h3>
+                            <textarea id='notification' rows="6" placeholder='Enter 200 characters or less' maxLength={200}></textarea>
+                            <label htmlFor='notification'></label>
+                        </div>
+                    </section>
 
-                        <section className='introduce_of_circle'>
-                            <div className='introduce_of_circle_cont'>
-                                <h3>⑤ Introduce of circle</h3>
-                                <textarea id='introduce' rows="16" placeholder='Enter 300 characters or less'></textarea>
-                                <label htmlFor='introduce'></label>
-                            </div>
-                        </section>
-                    </div>
+                    <section className='introduce_of_circle'>
+                        <div className='introduce_of_circle_cont'>
+                            <h3>⑤ Introduce of circle</h3>
+                            <textarea id='introduce' rows="18" placeholder='Enter 300 characters or less' maxLength={300}></textarea>
+                            <label htmlFor='introduce'></label>
+                        </div>
+                    </section>
 
                     <section className='mc_schedule'>
                         <Schedule/>
