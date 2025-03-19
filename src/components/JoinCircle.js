6acb3f4ad2c4cb9ab9130456aa03bd0591d897c4
sohyun <sohyun32253@kaywon.ac.kr>
2025-03-19 14:04:14 +0900

    '3.19jc'

--- src/components/JoinCircle.js
+++ src/components/JoinCircle.js
@@ -19,7 +19,6 @@ function JoinCircle() {
     const handleJoinBtn = () => {
         setJoinBtn((prev) => (prev === "Join the Club" ? "Leave the Club" : "Join the Club"))
     }
-  
         const settings = {
           dots: true,
           infinite: true,
@@ -54,104 +53,103 @@ function JoinCircle() {
             { name: "Peter Parker", language: "English", major: "Business", imgSrc: "" },
             { name: "Peter Parker", language: "English", major: "Business", imgSrc: "" }
         ]
-
     return(
     <main className='main sub_main'>
         <div className='join_circle_wrap'>
-            <h2>Join circle <button onClick={handleHeartClick}><img src={clickHeart} alt="찜하기"/></button></h2>
-            <div className='join_circle_cont1'>
-                <section className='intro'>
-                    <div className='intro_cont'> 
-                        <h3>Hello, chill guys~!</h3>
-                        <div className='tag_wrap'>
-                            <div className='category_tag'>
-                                category
-                            </div>
-                            <div className='charateristic_tag'>
-                                charateristic
-                            </div>
-                        </div>
-                        <p>contents</p>
-                    </div>
-                </section>
-                
-                <div className='btn_wrap'>
-                    <button className='join_btn' onClick={handleJoinBtn}>{joinBtn} <span>→</span></button>
-                    <button className='inquiry_btn'>1:1 inquiry <span>→</span></button>
-                </div>
-            </div>
-
-            <div className='join_circle_cont2'>
-                <section className='community_preview'>
-                    <div className='community_preview_cont'>
-                        <button className="go_community">자세히 보기</button>
-                        <div className="filter"></div> 
-                        <h3><img src={communityPreviewIcon} alt='미리보기'/>Community preview</h3>
-                        <div className="slider-container">
-                        <Slider {...settings}>
-                            {profiles.map((profile, index) => (
-                                <div className="profile_prev" key={index}>
-                                    <img src={profile.imgSrc} alt={`${profile.nickname} 프로필 이미지`} />
-                                    <dl>
-                                        <dt>
-                                            {profile.nickname} <time className="time">{profile.time}</time>
-                                        </dt>
-                                        <dd>{profile.text}</dd>
-                                    </dl>
+            <h2>Join circle <button onClick={handleHeartClick}><img src={clickHeart} alt="찜하기"/></button></h2>  
+            <div className='join_circle_cont'>
+                <div className="jc_container">
+                    <section className='intro'>
+                        <div className='intro_cont'> 
+                            <h3>Hello, chill guys~!</h3>
+                            <div className='tag_wrap'>
+                                <div className='category_tag'>
+                                    category
                                 </div>
+                                <div className='charateristic_tag'>
+                                    charateristic
+                                </div>
+                            </div>
+                            <p>contents</p>
+                        </div>
+                    </section>
+                
+                    <div className='btn_wrap'>
+                        <button className='join_btn' onClick={handleJoinBtn}>{joinBtn} <span>→</span></button>
+                        <button className='inquiry_btn'>1:1 inquiry <span>→</span></button>
+                    </div>
+                
+
+                    <section className='community_preview'>
+                        <div className='community_preview_cont'>
+                            <button className="go_community">자세히 보기</button>
+                            <div className="filter"></div> 
+                            <h3><img src={communityPreviewIcon} alt='미리보기'/>Community preview</h3>
+                            <div className="slider-container">
+                            <Slider {...settings}>
+                                {profiles.map((profile, index) => (
+                                    <div className="profile_prev" key={index}>
+                                        <img src={profile.imgSrc} alt={`${profile.nickname} 프로필 이미지`} />
+                                        <dl>
+                                            <dt>
+                                                {profile.nickname} <time className="time">{profile.time}</time>
+                                            </dt>
+                                            <dd>{profile.text}</dd>
+                                        </dl>
+                                    </div>
+                                ))}
+                            </Slider>
+                            </div>
+                        </div>
+                    </section>
+
+                    <section className='activity'>
+                        <div className='activity_cont'>
+                            <h3><img src={activityIcon} alt=''/>Activity</h3>
+                            {activities.map((activity, index) => (
+                            <div className='activ_box' key={index}>
+                                <img src={activity.imgSrc} alt="" />
+                                <ul>
+                                <li>{activity.contents}</li>
+                                <li>{activity.time}</li>
+                                </ul>
+                            </div>
                             ))}
-                        </Slider>
                         </div>
-                    </div>
-                </section>
+                    </section>
 
-                <section className='activity'>
-                    <div className='activity_cont'>
-                        <h3><img src={activityIcon} alt=''/>Activity</h3>
-                        {activities.map((activity, index) => (
-                        <div className='activ_box' key={index}>
-                            <img src={activity.imgSrc} alt="" />
-                            <ul>
-                               <li>{activity.contents}</li>
-                               <li>{activity.time}</li>
-                            </ul>
+                    <section className='notification jc_notification'>
+                        <div className='notification_cont'>
+                            <h3><img src={notificationIcon} alt=''/>Notification</h3>
+                            <p>contents</p>
                         </div>
-                        ))}
-                    </div>
-                </section>
-            </div>
+                    </section>
 
-            <div className='join_circle_cont3'>
-                <section className='notification jc_notification'>
-                    <div className='notification_cont'>
-                        <h3><img src={notificationIcon} alt=''/>Notification</h3>
-                        <p>contents</p>
-                    </div>
-                </section>
-                <section className='schedule'>
-                    <div className='schedule_cont'>
-                        <Schedule/>
-                    </div>
-                </section>
-            </div>
+                    <section className='schedule'>
+                        <div className='schedule_cont'>
+                            <Schedule/>
+                        </div>
+                    </section>
            
-            <section className='people'>
-                <div className='people_cont'>
-                    <h3><img src={peopleIcon} alt=''/>People</h3>
-                <div className="p_container">
-                    {joiner.map((people,index) => (
-                    <div className='people_box' key={index}>
-                        <img src="" alt="" />
-                        <ul>
-                            <li className='p_name'>{people.name}</li>
-                            <li className='p_lang'>{people.language}</li>
-                            <li className='p_major'>{people.major}</li>
-                        </ul>
-                    </div>
-                    ))}
-                    </div>
+                    <section className='people'>
+                        <div className='people_cont'>
+                            <h3><img src={peopleIcon} alt=''/>People</h3>
+                        <div className="p_container">
+                            {joiner.map((people,index) => (
+                            <div className='people_box' key={index}>
+                                <img src="" alt="" />
+                                <ul>
+                                    <li className='p_name'>{people.name}</li>
+                                    <li className='p_lang'>{people.language}</li>
+                                    <li className='p_major'>{people.major}</li>
+                                </ul>
+                            </div>
+                            ))}
+                            </div>
+                        </div>
+                    </section>
                 </div>
-            </section>
+            </div>
         </div>
     </main>
     )
