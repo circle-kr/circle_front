96e1e4ebce4f1615b7a026b7d1c058ff0286e11a
sohyun <sohyun32253@kaywon.ac.kr>
2025-03-26 11:50:39 +0900

    '11:50'

--- src/components/JoinedCircle01.js
+++ src/components/JoinedCircle01.js
@@ -1,9 +1,70 @@
+import { useState,useRef,useEffect} from 'react';
 import React from 'react';
 import '../JoinedCircle.css';
+import Schedule from './Schedule';
+import PostPopUp from './PostPopUp';
 import notificationIcon from '../images/notification_icon.svg'
-import feedIcon from '../images/feed_write_icon.svg'
-import editIcon from '../images/edit_icon.svg'
+import feedIcon from '../images/chat_icon.svg'
+import feedWriteIcon from '../images/feed_write_icon.svg'
+import moreIcon from '../images/more_icon.svg'
+import closeIcon from '../images/close_icon_black.svg'
+import unlikedIcon from '../images/favorite_stroke_black_icon.svg'
+import likedIcon from '../images/favorite_black_icon.svg'
+import commentIcon from '../images/chat_icon.svg'
+
 function JoinedCircle01() {
+    const categoryBtn = ["category"];
+    const charateristicBtn = ["charateristic1","charateristic2"];
+    const [isPopupOpen, setIsPopupOpen] = useState(false);
+    const [isFollowed, setIsFollowed] = useState(false);
+    const [isMoreVisible, setIsMoreVisible] = useState(false); // State for popup visibility
+    const [isLiked, setIsLiked] = useState(unlikedIcon);
+    const [isCommentLiked, setIsCommentLiked] = useState(unlikedIcon);
+
+    const followToggle = () => {
+        setIsFollowed(!isFollowed)
+    }
+
+    const morePopUpClick = (event) => {
+        event.stopPropagation(); // 이벤트 버블링 방지
+        setIsMoreVisible(!isMoreVisible)
+    }
+
+    const closePopup = () => {
+        setIsMoreVisible(false);
+      }
+
+    const popupRef = useRef(null);
+    // 팝업 외부 클릭 시 팝업 닫기
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+        console.log("부모")
+        setTimeout(() => closePopup(), 0); // 0ms 딜레이 후 실행
+      if (popupRef.current && !popupRef.current.contains(event.target)) {
+        closePopup(); // 외부 클릭 시 팝업 닫기
+      }
+    };
+
+    // 이벤트 리스너 추가
+    document.addEventListener('click', handleClickOutside);
+
+    // 클린업 함수
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
+
+  
+
+    const likedToggle = () => {
+        setIsLiked(isLiked === unlikedIcon ? likedIcon : unlikedIcon )
+    }
+
+    const commentLiked = () => {
+        setIsCommentLiked(isCommentLiked === unlikedIcon ? likedIcon : unlikedIcon )
+    }
+
     return(
     <main className='main sub_main'>
         <div className='joined_circle_wrap'>
@@ -11,13 +72,24 @@ function JoinedCircle01() {
             <div className='joined_circle_cont'>
                 <section className='intro'>
                     <div className='intro_cont'>
-                        
+                        <h3>Hello, chill guys~!</h3>
+                        <div className='tag_wrap'>
+                            <div className='category_tag'>
+                                {categoryBtn}
+                            </div>
+                            {charateristicBtn.map( charateristic => (
+                            <div className='charateristic_tag' key={charateristic}>
+                                {charateristic}
+                            </div>
+                            ))}
+                        </div>
+                        <p>contents</p>
                     </div>
                 </section>
 
                 <section className="schedule">
                    <div className="schedule_cont">
-                       
+                       <Schedule />
                    </div>
                 </section>
 
@@ -29,12 +101,53 @@ function JoinedCircle01() {
 
                 <section className='feed'>
                     <div className='feed_cont'>
-                        <h3><img src={feedIcon} alt=''/>Feed <img src={editIcon} alt='편집하기'/></h3>
+                        <h3><img src={feedIcon} alt=''/>Feed </h3><button className='feed_write' onClick={() => setIsPopupOpen(true)}><img src={feedWriteIcon} alt='글쓰기'/></button>
+                        {isPopupOpen && <PostPopUp ref={popupRef} onClose={() => setIsPopupOpen(false)}  />}
+                        <div className='sns_box'>
+                            <div className='sns_top'>
+                                <img src="" alt="" className='profile_img'/>
+                                <h4>nickname</h4>
+                                <div className='top_btn_wrap'>
+                                    <button className={`follow ${isFollowed ? 'fw_clicked' : ""}`} onClick={followToggle} key={isFollowed}>Follow</button>
+                                    <button onClick={morePopUpClick} className='sns_more'><img src={moreIcon} alt="" /></button>
+                                </div>
+                            </div>
+
+                            <>
+                            {isMoreVisible === true &&(
+                            <div className='popup_wrap' ref={popupRef}>
+                                <button onClick={closePopup}><img src={closeIcon} alt="닫기" /></button>
+                                <p>Edit</p>
+                                <p>Delete</p>
+                            </div>
+                            )} 
+                            </>     
+
+                            <div className='sns_bottom'>
+                                <div className='sns_img'>
+                                    <img src="" alt="" />
+                                </div>
+                                <div className='bottom_btn_wrap'>
+                                    <button className='sns_like' onClick={likedToggle}><img src={isLiked} alt="좋아요" /></button>
+                                    <button className='sns_comment'><img src={commentIcon} alt="댓글" /></button>
+                                </div>
+
+                                <div className='comment_cont'>
+                                    <p><span>nickname1</span>님 외 여러 명이 좋아합니다.</p>
+                                    <p className='comm_align'><span>nickname2</span> comments1
+                                    <button onClick={commentLiked} className='comm_like'>
+                                        <img src={isCommentLiked} alt="댓글 좋아요" />
+                                    </button>
+                                    </p>
+                                </div>
+                                <time>11월 18일</time>
+                            </div>
+                        </div>
                     </div>
                 </section>
+           
             </div>
         </div>
     </main>
-    )
-}
+    )}
 export default JoinedCircle01;
---
+++ src/components/PostPopUp.js
@@ -0,0 +1,60 @@
+import React, {useEffect, useRef,useState,forwardRef} from 'react'
+import '../PostPopUp.css'
+import closeIcon from '../images/close_icon_black.svg'
+import photoIcon from '../images/photo_icon.svg'
+
+const PostPopUp = forwardRef(({onClose},popupRef) => {
+    const [ text, setText ] = useState('');
+    const handleInputChange = (e) => {
+        setText(e.target.value);
+    }
+    const textareaRef = useRef(null);
+    const fileInputRef = useRef(null);
+
+    useEffect( () => {
+        if(textareaRef.current){
+            textareaRef.current.style.height = 'auto';
+            textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
+        }
+    })
+
+    const handleClick = () => {
+        fileInputRef.current.click(); // 파일 업로드 input 클릭 트리거
+      };
+    
+  return (
+    <div className="popup-overlay">
+      <div className="popup-content" ref={popupRef}>
+      <button onClick={onClose} className='post_close'><img src={closeIcon} alt="" /></button>
+        <div className='popup_top'>
+            <div><img src="" alt="" /></div>
+            <textarea 
+            onChange={handleInputChange}
+            placeholder="Share your daily life!" 
+            />
+        </div>
+        
+        <div className='popup_bottom'>
+             {/* 숨겨진 파일 업로드 input */}
+            <input 
+                type="file" 
+                ref={fileInputRef} 
+                style={{ display: "none" }} 
+            />
+            
+            {/* 이미지 클릭 시 파일 업로드 실행 */}
+            <img 
+                src={photoIcon} 
+                alt="사진 추가" 
+                onClick={handleClick} 
+                style={{ cursor: "pointer" }} 
+                className='post_photo'
+            />
+            <button className='post_btn'>Post</button>
+        </div>
+      </div>
+    </div>
+  );
+});
+
+export default PostPopUp;
