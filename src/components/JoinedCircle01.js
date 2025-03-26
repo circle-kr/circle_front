1f0a861d259d0a3f7b4ada5be787d9e82de23e75
sohyun <sohyun32253@kaywon.ac.kr>
2025-03-26 09:50:23 +0900

    '3.26'

--- src/components/JoinedCircle01.js
+++ src/components/JoinedCircle01.js
@@ -1,28 +1,41 @@
+import { useState,useRef,useEffect } from 'react';
 import React from 'react';
 import '../JoinedCircle.css';
 import Schedule from './Schedule';
+import PostPopUp from './PostPopUp';
 import notificationIcon from '../images/notification_icon.svg'
-import feedIcon from '../images/feed_write_icon.svg'
-import editIcon from '../images/edit_icon.svg'
+import feedIcon from '../images/chat_icon.svg'
+import feedWriteIcon from '../images/feed_write_icon.svg'
 import moreIcon from '../images/more_icon.svg'
 import closeIcon from '../images/close_icon_black.svg'
-import photoIcon from '../images/photo_icon.svg'
-import favoriteIcon from '../images/favorite_stroke_icon.svg'
+import unlikedIcon from '../images/favorite_stroke_icon.svg'
+import likedIcon from '../images/favorite_icon.svg'
 import commentIcon from '../images/chat_icon.svg'
-import { useState,useRef,useEffect } from 'react';
 
 function JoinedCircle01() {
     const categoryBtn = ["category"];
     const charateristicBtn = ["charateristic1","charateristic2"];
-    const [isPopupVisible, setIsPopupVisible] = useState(false); // State for popup visibility
-    const popupClick = () => {
-        setIsPopupVisible(!isPopupVisible)
+    const [isPopupOpen, setIsPopupOpen] = useState(false);
+    const [isFollowed, setIsFollowed] = useState(false);
+    const [isMoreVisible, setIsMoreVisible] = useState(false); // State for popup visibility
+    const [isLiked, setIsLiked] = useState(unlikedIcon);
+    const [isCommentLiked, setIsCommentLiked] = useState(unlikedIcon);
+
+    const followToggle = () => {
+        setIsFollowed(!isFollowed)
     }
+
+    const morePopUpClick = () => {
+        setIsMoreVisible(!isMoreVisible)
+    }
+
     const closePopup = () => {
-        setIsPopupVisible(false);
+        setIsMoreVisible(false);
       }
-      const popupRef = useRef(null);
-       // 팝업 외부 클릭 시 팝업 닫기
+
+    const popupRef = useRef(null);
+    // 팝업 외부 클릭 시 팝업 닫기
+
   useEffect(() => {
     const handleClickOutside = (event) => {
       if (popupRef.current && !popupRef.current.contains(event.target)) {
@@ -38,6 +51,14 @@ function JoinedCircle01() {
       document.removeEventListener('mousedown', handleClickOutside);
     };
   }, []);
+
+    const likedToggle = () => {
+        setIsLiked(isLiked === unlikedIcon ? likedIcon : unlikedIcon )
+    }
+
+    const commentLiked = () => {
+        setIsCommentLiked(isCommentLiked === unlikedIcon ? likedIcon : unlikedIcon )
+    }
 
     return(
     <main className='main sub_main'>
@@ -75,19 +96,20 @@ function JoinedCircle01() {
 
                 <section className='feed'>
                     <div className='feed_cont'>
-                        <h3><img src={feedIcon} alt=''/>Feed <img src={editIcon} alt='편집하기'/></h3>
+                        <h3><img src={feedIcon} alt=''/>Feed </h3><button className='feed_write' onClick={() => setIsPopupOpen(true)}><img src={feedWriteIcon} alt='글쓰기'/></button>
+                        {isPopupOpen && <PostPopUp onClose={() => setIsPopupOpen(false)} ref={popupRef} />}
                         <div className='sns_box'>
                             <div className='sns_top'>
                                 <img src="" alt="" className='profile_img'/>
                                 <h4>nickname</h4>
                                 <div className='top_btn_wrap'>
-                                    <button className='follow'>Follow</button>
-                                    <button onClick={popupClick}><img src={moreIcon} alt="" /></button>
+                                    <button className={`follow ${isFollowed ? 'fw_clicked' : ""}`} onClick={followToggle} key={isFollowed}>Follow</button>
+                                    <button onClick={morePopUpClick} className='sns_more'><img src={moreIcon} alt="" /></button>
                                 </div>
                             </div>
 
                             <>
-                            {isPopupVisible === true &&(
+                            {isMoreVisible === true &&(
                             <div className='popup_wrap' ref={popupRef}>
                                 <button onClick={closePopup}><img src={closeIcon} alt="닫기" /></button>
                                 <p>Edit</p>
@@ -101,12 +123,18 @@ function JoinedCircle01() {
                                     <img src="" alt="" />
                                 </div>
                                 <div className='bottom_btn_wrap'>
-                                    <button><img src={favoriteIcon} alt="" /></button>
-                                    <button><img src={commentIcon} alt="" /></button>
+                                    <button className='sns_like' onClick={likedToggle}><img src={isLiked} alt="좋아요" /></button>
+                                    <button className='sns_comment'><img src={commentIcon} alt="댓글" /></button>
                                 </div>
-                                <p><span>nickname1</span>님 외 여러 명이 좋아합니다.</p>
-                                <p><span>nickname2</span> comments1</p>
-                                <p><span>nickname3</span> comments2</p>
+
+                                <div className='comment_cont'>
+                                    <p><span>nickname1</span>님 외 여러 명이 좋아합니다.</p>
+                                    <p className='comm_align'><span>nickname2</span> comments1
+                                    <button onClick={commentLiked} className='comm_like'>
+                                        <img src={isCommentLiked} alt="댓글 좋아요" />
+                                    </button>
+                                    </p>
+                                </div>
                                 <time>11월 18일</time>
                             </div>
                         </div>
